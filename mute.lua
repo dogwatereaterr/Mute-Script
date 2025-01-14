3c375d64717acd0669fcee67790b8ad8dadcf230
dogwatereaterr <bostonkinloch@gmail.com>
2025-01-14 18:28:38 +1300

    Update mute.lua

--- mute.lua
+++ mute.lua
@@ -146,7 +146,7 @@ local function Execute()
   game.ReplicatedStorage.Remotes.Messenger:FireServer("/cmd mute " .. ID .. " " .. Length)
 
   if math.floor(Length/3600) >= 1 then
-    LengthString = tostring(math.floor(Length/3600)) .. "h " .. (tostring(math.floor(Length/60) - 60*math.floor(Length/3600))) .. "m " .. tostring(Length - 60*math.floor(Length/60) - 3600*math.floor(Length/3600)) .. "s"
+    LengthString = tostring(math.floor(Length/3600)) .. "h " .. (tostring(math.floor(Length/60) - 60*math.floor(Length/3600))) .. "m " .. tostring(Length - 3600*math.floor(Length/3600) - (60*math.floor(Length/60) - 3600*math.floor(Length/3600))) .. "s"
   else
     LengthString = tostring(math.floor(Length/60)) .. "m " .. tostring(Length - 60*math.floor(Length/60))
   end
