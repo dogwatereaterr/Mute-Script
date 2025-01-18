ff0d4ca816693f28bde4493aff2e4c9c721720be
dogwatereaterr <bostonkinloch@gmail.com>
2025-01-19 11:52:11 +1300

    Update mute.lua

--- mute.lua
+++ mute.lua
@@ -19,7 +19,7 @@ end
 local UI_Holder = Instance.new("ScreenGui", Player1.PlayerGui)
 UI_Holder.Name = "Mut3r"
 
-print("v5.0.14")
+print("v5.0.17")
 
 --Define Main System
 --------------------------------------------------------------------
